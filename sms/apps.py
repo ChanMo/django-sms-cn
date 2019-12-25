3a6ec91133647aa6f95285aa10474d81fd3f307f
dyou <chen.orange@aliyun.com>
2019-12-25 15:51:55 +0800

    remove receivers

--- sms/apps.py
+++ sms/apps.py
@@ -5,6 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class SmsConfig(AppConfig):
     name = 'sms'
     verbose_name = _('sms')
-
-    def ready(self):
-        import sms.receivers
